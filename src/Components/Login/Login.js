b6d83c10c47dcf0668087aebdf449d8c05cdd4af
RKZeebon <rkzeebon@gmail.com>
2022-05-01 02:35:45 +0600

    design edit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
                     <div>
                         <input className='text-xl my-3 border-2 border-black w-full p-2 rounded-md' type="password" name="pass" id="" placeholder='Enter your password' />
                     </div>
-                    <div className='bg-blue-400 py-2 text-center my-5 rounded-md hover:bg-[royalblue] hover:text-white cursor-pointer'><input className='text-2xl font-Roboto font-semibold cursor-pointer' type="submit" value="Login" /></div>
+                    <div className='bg-blue-400 py-2 text-center my-5 rounded-md hover:bg-[royalblue] hover:text-white cursor-pointer shadow-md shadow-[royalblue]'><input className='text-2xl font-Roboto font-semibold cursor-pointer' type="submit" value="Login" /></div>
                 </form>
                 <p className='text-lg text-center'>New here? Please <Link className='text-[royalblue] font-semibold' to='/register'>Register</Link></p>
                 <div className='flex items-center justify-center mt-8'>
@@ -24,10 +24,10 @@ const Login = () => {
                     <div className='bg-blue-400 h-[2px] ml-1 w-full'></div>
                 </div>
                 <div>
-                    <div className='text-center bg-blue-400 my-3 py-2 rounded-md'>
+                    <div className='text-center bg-blue-400 my-3 py-2 rounded-md shadow-md shadow-[royalblue] hover:bg-[royalblue] hover:text-white cursor-pointer'>
                         <button className='text-xl font-semibold'> <img className='inline-block' src={gLogo} alt="" /> Continue With Google</button>
                     </div>
-                    <div className='text-center bg-blue-400 my-3 py-2 rounded-md'>
+                    <div className='text-center bg-blue-400 my-3 py-2 rounded-md shadow-md shadow-[royalblue] hover:bg-[royalblue] hover:text-white cursor-pointer'>
                         <button className='text-xl font-semibold'> <img className='inline-block' src={gitLogo} alt="" /> Continue With Github</button>
                     </div>
                 </div>
