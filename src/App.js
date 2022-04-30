c55cf566c61990d7591856c112c5c7c699e77c55
RKZeebon <rkzeebon@gmail.com>
2022-05-01 03:40:43 +0600

    register page design done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './Components/Home/Home';
 import Inventory from './Components/Inventory/Inventory';
 import Login from './Components/Login/Login';
 import ManageInventory from './Components/ManageInventory/ManageInventory';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/home' element={<Home />}></Route>
         <Route path='/inventory' element={<Inventory />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
         <Route path='/inventory/:id' element={<Details />}></Route>
         <Route path='/manage-inventory' element={<ManageInventory />}></Route>
         <Route path='/add-item' element={<AddItem />}></Route>
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import gLogo from '../../Assets/google-logo.ico'
 import gitLogo from '../../Assets/git-logo.ico'
-const Login = () => {
+import { Link } from 'react-router-dom';
+const Register = () => {
     return (
         <div className='min-h-[850px]'>
 
             <div className='w-2/3 lg:w-1/3 mx-auto my-8 pt-5 pb-10 shadow-2xl shadow-blue-400 px-2 rounded-lg'>
-                <h1 className='text-3xl font-Roboto font-semibold text-[royalblue] text-center'>Please login</h1>
+                <h1 className='text-3xl font-Roboto font-semibold text-[royalblue] text-center'>Please Sign Up</h1>
                 <form>
+                    <div>
+                        <input className='text-xl my-3 border-2 border-black w-full p-2 rounded-md' type="text" name="name" id="" placeholder='Enter Your Name' />
+                    </div>
                     <div>
                         <input className='text-xl my-3 border-2 border-black w-full p-2 rounded-md' type="email" name="email" id="" placeholder='Enter Your Email' />
                     </div>
                     <div>
-                        <input className='text-xl my-3 border-2 border-black w-full p-2 rounded-md' type="password" name="pass" id="" placeholder='Enter Your Password' />
+                        <input className='text-xl my-3 border-2 border-black w-full p-2 rounded-md' type="password" name="pass" id="" placeholder='Enter Password' />
                     </div>
-                    <div className='bg-blue-400 text-center my-5 rounded-md hover:bg-[royalblue] hover:text-white cursor-pointer shadow-md shadow-[royalblue]'><input className='text-2xl font-Roboto font-semibold cursor-pointer w-full py-2' type="submit" value="Login" /></div>
+                    <div>
+                        <input className='text-xl my-3 border-2 border-black w-full p-2 rounded-md' type="password" name="confirmPass" id="" placeholder='Confirm Password' />
+                    </div>
+                    <div className='bg-blue-400 text-center my-5 rounded-md hover:bg-[royalblue] hover:text-white cursor-pointer shadow-md shadow-[royalblue]'><input className='text-2xl font-Roboto font-semibold cursor-pointer w-full py-2' type="submit" value="Sign Up" /></div>
                 </form>
-                <p className='text-lg text-center'>New here? Please <Link className='text-[royalblue] font-semibold' to='/register'>Register</Link></p>
+                <p className='text-lg text-center'>Already have an account? <Link className='text-[royalblue] font-semibold' to='/login'>Login</Link></p>
                 <div className='flex items-center justify-center mt-8'>
                     <div className='bg-blue-400 h-[2px] mr-1 w-full'></div>
                     <p>Or</p>
@@ -36,4 +42,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
